ec48ea2f5cb0bab399fd7f72045e5e4060a9f25e
Александр Сысоев <alexgood5000@gmail.com>
2025-04-24 05:19:58 +0300

    Изменил Link на NavLink в шапке

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -7,76 +7,68 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
-import { Link, useLocation } from 'react-router-dom';
-
-enum LinkNumbers {
-  Constructor = 1,
-  Feed = 2,
-  Profile = 3
-}
+import { NavLink, useLocation } from 'react-router-dom';
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
   const { pathname } = useLocation();
-  let linkNumber = 0;
-
-  if (pathname === '/' || pathname.includes('/ingredients')) {
-    linkNumber = LinkNumbers.Constructor;
-  } else if (pathname.includes('/feed')) {
-    linkNumber = LinkNumbers.Feed;
-  } else if (pathname.includes('/profile')) {
-    linkNumber = LinkNumbers.Profile;
-  }
+  const isIngredientsPath = pathname.includes('/ingredients');
 
   return (
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
         <div className={styles.menu_part_left}>
-          <Link
-            className={`${styles.link} ${
-              linkNumber === LinkNumbers.Constructor ? styles.link_active : ''
-            }`}
+          <NavLink
+            className={({ isActive }) =>
+              `${styles.link} ${isActive || isIngredientsPath ? styles.link_active : ''}`
+            }
             to='/'
           >
-            <BurgerIcon
-              type={
-                linkNumber === LinkNumbers.Constructor ? 'primary' : 'secondary'
-              }
-            />
-            <p className='text text_type_main-default ml-2 mr-10'>
-              Конструктор
-            </p>
-          </Link>
-          <Link
-            className={`${styles.link} ${
-              linkNumber === LinkNumbers.Feed ? styles.link_active : ''
-            }`}
+            {({ isActive }) => (
+              <>
+                <BurgerIcon
+                  type={isActive || isIngredientsPath ? 'primary' : 'secondary'}
+                />
+                <p className='text text_type_main-default ml-2 mr-10'>
+                  Конструктор
+                </p>
+              </>
+            )}
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.link_active : ''}`
+            }
             to='/feed'
           >
-            <ListIcon
-              type={linkNumber === LinkNumbers.Feed ? 'primary' : 'secondary'}
-            />
-            <p className='text text_type_main-default ml-2'>Лента заказов</p>
-          </Link>
+            {({ isActive }) => (
+              <>
+                <ListIcon type={isActive ? 'primary' : 'secondary'} />
+                <p className='text text_type_main-default ml-2'>
+                  Лента заказов
+                </p>
+              </>
+            )}
+          </NavLink>
         </div>
         <div className={styles.logo}>
           <Logo className='' />
         </div>
         <div className={styles.link_position_last}>
-          <Link
-            className={`${styles.link} ${
-              linkNumber === LinkNumbers.Profile ? styles.link_active : ''
-            }`}
+          <NavLink
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.link_active : ''}`
+            }
             to='/profile'
           >
-            <ProfileIcon
-              type={
-                linkNumber === LinkNumbers.Profile ? 'primary' : 'secondary'
-              }
-            />
-            <p className='text text_type_main-default ml-2'>
-              {userName || 'Личный кабинет'}
-            </p>
-          </Link>
+            {({ isActive }) => (
+              <>
+                <ProfileIcon type={isActive ? 'primary' : 'secondary'} />
+                <p className='text text_type_main-default ml-2'>
+                  {userName || 'Личный кабинет'}
+                </p>
+              </>
+            )}
+          </NavLink>
         </div>
       </nav>
     </header>
